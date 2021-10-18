e67a0ad397072b3e752ea43e472797618504d1ce
pjover.it <pjover@hotelbeds.com>
2021-10-18 07:58:17 +0200

    Add version flag

--- cmd/root.go
+++ cmd/root.go
@@ -33,6 +33,7 @@ var rootCmd = &cobra.Command{
 	Short: "A Command Line Interface to Hobbit service",
 	Long: `A Command Line Interface to Hobbit service in Go.
 	Complete documentation is available at https://github.com/pjover/sam`,
+	Version: comm.Version,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -46,18 +47,7 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sam.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
-	fmt.Printf("Sam v%s\n", comm.Version)
 }
 
 // initConfig reads in config file and ENV variables if set.
