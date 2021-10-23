001ac4754b9243a319c4e24af5ca4eebbad1d3c0
Pere Jover <pjover@gmail.com>
2021-10-23 07:09:47 +0200

    cobra add mcli --config ./.cobra.yaml

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,3 @@
-/*
-Copyright © 2021 Pere Jover
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
 package cmd
 
 import (
@@ -36,9 +20,6 @@ var rootCmd = &cobra.Command{
 	Long: `A Command Line Interface to Hobbit service in Go.
 	Complete documentation is available at https://github.com/pjover/sam`,
 	Version: comm.Version,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
