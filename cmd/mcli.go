48653e278791aa6257a3375b63dbb431e1d884d5
Pere Jover <pjover@gmail.com>
2021-10-24 21:48:59 +0200

    mcli without flag

--- cmd/mcli.go
+++ cmd/mcli.go
@@ -1,42 +1,28 @@
 package cmd
 
 import (
-	"fmt"
 	"sam/adm"
+	"sam/comm"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
-var customerCode int
-var childCode int
-
 var mcliCmd = &cobra.Command{
-	Use:   "mcli {-c 123 | -i 1230}",
-	Short: "Mostra les dades d'un client",
-	Long:  "Mostra les dades d'un client, se pot indicar el client o l'infant",
-	Example: `   mcli -i 1520    Mostra les dades del client de l'infant 1520
-   mcli -c 152     Mostra les dades del client 152`,
+	Use:         "mcli codiClient",
+	Short:       "Mostra les dades d'un client",
+	Long:        "Mostra les dades d'un client indicant el seu codi",
+	Example:     `   mcli 152     Mostra les dades del client 152`,
 	Annotations: map[string]string{"ADM": "Comandes d'adminitració"},
+	Args: func(cmd *cobra.Command, args []string) error {
+		return comm.ValidateCode(args)
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return runMcli()
+		customerCode, _ := strconv.Atoi(args[0])
+		return adm.DisplayCustomer(customerCode)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(mcliCmd)
-	mcliCmd.Flags().IntVarP(&customerCode, "client", "c", 0, "Codi del client")
-	mcliCmd.Flags().IntVarP(&childCode, "infant", "i", 0, "Codi de l'infant")
-}
-
-func runMcli() error {
-	if childCode > 0 && customerCode > 0 {
-		return fmt.Errorf("Indicar el codi del client o del infant, no els dos a l'hora")
-	} else if childCode == 0 && customerCode == 0 {
-		return fmt.Errorf("És necesari indicar el codi del client o del infant")
-	}
-
-	if childCode > 0 {
-		customerCode = childCode / 10
-	}
-	return adm.DisplayCustomer(customerCode)
 }
