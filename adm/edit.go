cc6184ef1fc084021b33db9d5634ca376149e75d
Pere Jover <pjover@gmail.com>
2021-10-26 20:06:08 +0200

    Edit invoice

--- adm/edit.go
+++ adm/edit.go
@@ -18,3 +18,10 @@ func EditProduct(productCode string) error {
 	fmt.Println("Editant el producte", productCode, "a", _url)
 	return comm.OpenUrl(_url)
 }
+
+func EditInvoice(invoiceCode string) error {
+	code := url.QueryEscape(fmt.Sprintf("\"%s\"", invoiceCode))
+	_url := fmt.Sprintf("http://localhost:8081/db/hobbit_prod/invoice/%s", code)
+	fmt.Println("Editant la factura", invoiceCode, "a", _url)
+	return comm.OpenUrl(_url)
+}
