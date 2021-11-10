78fd4295057ef22d0ae32a8e2037936e96256057
Pere Jover <pjover@gmail.com>
2021-11-10 20:57:29 +0100

    Refactor test

--- cmd/insertConsumptions_test.go
+++ cmd/insertConsumptions_test.go
@@ -2,66 +2,81 @@ package cmd
 
 import (
 	"errors"
-	"github.com/stretchr/testify/assert"
+	"reflect"
 	"sam/core"
 	"testing"
 )
 
 var testNote = "Test note"
 
-func Test_ParseInsertConsumptionsArgs(t *testing.T) {
-
-	t.Run("Should fail with invalid child code", func(t *testing.T) {
-		var actual, err = parseInsertConsumptionsArgs([]string{"0.5", "MME", "2", "MME"}, testNote)
-		assert.Equal(t, core.InsertConsumptionsArgs{}, actual)
-		assert.Equal(t, errors.New("El codi d'infant introduit és invàlid: 0.5"), err)
-	})
-
-	t.Run("Should fail with invalid unit format", func(t *testing.T) {
-		var actual, err = parseInsertConsumptionsArgs([]string{"2220", "0,5", "MME", "2", "AGE"}, testNote)
-		assert.Equal(t, core.InsertConsumptionsArgs{}, actual)
-		assert.Equal(t, errors.New("El número introduit és invàlid: 0,5"), err)
-	})
-
-	t.Run("Should fail with invalid unit format", func(t *testing.T) {
-		var actual, err = parseInsertConsumptionsArgs([]string{"2220", "MME", "2"}, testNote)
-		assert.Equal(t, core.InsertConsumptionsArgs{}, actual)
-		assert.Equal(t, errors.New("El número introduit és invàlid: MME"), err)
-	})
-
-	t.Run("Should fail with invalid product code", func(t *testing.T) {
-		var actual, err = parseInsertConsumptionsArgs([]string{"2220", "2", "MMME"}, testNote)
-		assert.Equal(t, core.InsertConsumptionsArgs{}, actual)
-		assert.Equal(t, errors.New("El codi de producte introduit és invàlid: MMME"), err)
-	})
-
-	t.Run("Should fail without last product code", func(t *testing.T) {
-		var actual, err = parseInsertConsumptionsArgs([]string{"2220", "0.5", "MME", "2"}, testNote)
-		assert.Equal(t, core.InsertConsumptionsArgs{}, actual)
-		assert.Equal(t, errors.New("No s'ha indroduit el codi del darrer producte"), err)
-	})
-
-	t.Run("Should fail with duplicated product code", func(t *testing.T) {
-		var actual, err = parseInsertConsumptionsArgs([]string{"2220", "0.5", "MME", "2", "MME"}, testNote)
-		assert.Equal(t, core.InsertConsumptionsArgs{}, actual)
-		assert.Equal(t, errors.New("Hi ha un codi de producte repetit"), err)
-	})
-
-	t.Run("Should parse with 1 consumption", func(t *testing.T) {
-		var actual, err = parseInsertConsumptionsArgs([]string{"1552", "1", "QME"}, testNote)
-		assert.Equal(t, core.InsertConsumptionsArgs{Code: 1552, Consumptions: map[string]float64{"QME": 1}, Note: testNote}, actual)
-		assert.Nil(t, err)
-	})
-
-	t.Run("Should parse with 2 consumption", func(t *testing.T) {
-		var actual, err = parseInsertConsumptionsArgs([]string{"2220", "0.5", "MME", "2", "AGE"}, testNote)
-		assert.Equal(t, core.InsertConsumptionsArgs{Code: 2220, Consumptions: map[string]float64{"MME": 0.5, "AGE": 2}, Note: testNote}, actual)
-		assert.Nil(t, err)
-	})
-
-	t.Run("Should parse with 3 consumption", func(t *testing.T) {
-		var actual, err = parseInsertConsumptionsArgs([]string{"2220", "0.5", "MME", "2", "AGE", "1", "QME", "1", "BAB"}, testNote)
-		assert.Equal(t, core.InsertConsumptionsArgs{Code: 2220, Consumptions: map[string]float64{"MME": 0.5, "AGE": 2, "QME": 1, "BAB": 1}, Note: testNote}, actual)
-		assert.Nil(t, err)
-	})
+func Test_parseInsertConsumptionsArgs(t *testing.T) {
+	type args struct {
+		args    []string
+		noteArg string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    core.InsertConsumptionsArgs
+		wantErr error
+	}{
+		{
+			"Should fail with invalid child code",
+			args{[]string{"0.5", "MME", "2", "MME"}, testNote},
+			core.InsertConsumptionsArgs{},
+			errors.New("El codi d'infant introduit és invàlid: 0.5"),
+		}, {
+			"Should fail with invalid unit format",
+			args{[]string{"2220", "0,5", "MME", "2", "AGE"}, testNote},
+			core.InsertConsumptionsArgs{},
+			errors.New("El número introduit és invàlid: 0,5"),
+		}, {
+			"Should fail with invalid unit format",
+			args{[]string{"2220", "MME", "2"}, testNote},
+			core.InsertConsumptionsArgs{},
+			errors.New("El número introduit és invàlid: MME"),
+		}, {
+			"Should fail with invalid product code",
+			args{[]string{"2220", "2", "MMME"}, testNote},
+			core.InsertConsumptionsArgs{},
+			errors.New("El codi de producte introduit és invàlid: MMME"),
+		}, {
+			"Should fail without last product code",
+			args{[]string{"2220", "0.5", "MME", "2"}, testNote},
+			core.InsertConsumptionsArgs{},
+			errors.New("No s'ha indroduit el codi del darrer producte"),
+		}, {
+			"Should fail with duplicated product code",
+			args{[]string{"2220", "0.5", "MME", "2", "MME"}, testNote},
+			core.InsertConsumptionsArgs{},
+			errors.New("Hi ha un codi de producte repetit"),
+		}, {
+			"Should parse with 1 consumption",
+			args{[]string{"1552", "1", "QME"}, testNote},
+			core.InsertConsumptionsArgs{Code: 1552, Consumptions: map[string]float64{"QME": 1}, Note: testNote},
+			nil,
+		}, {
+			"Should parse with 2 consumptions",
+			args{[]string{"2220", "0.5", "MME", "2", "AGE"}, testNote},
+			core.InsertConsumptionsArgs{Code: 2220, Consumptions: map[string]float64{"MME": 0.5, "AGE": 2}, Note: testNote},
+			nil,
+		}, {
+			"Should parse with 3 consumptions",
+			args{[]string{"2220", "0.5", "MME", "2", "AGE", "1", "QME", "1", "BAB"}, testNote},
+			core.InsertConsumptionsArgs{Code: 2220, Consumptions: map[string]float64{"MME": 0.5, "AGE": 2, "QME": 1, "BAB": 1}, Note: testNote},
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseInsertConsumptionsArgs(tt.args.args, tt.args.noteArg)
+			if (err != nil) && !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("parseInsertConsumptionsArgs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseInsertConsumptionsArgs() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
