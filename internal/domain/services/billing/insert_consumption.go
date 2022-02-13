99cb4db5f72908ae97f4854dea2be81301d8c489
Pere Jover <pjover@gmail.com>
2022-02-13 06:36:19 +0100

    insertConsumptions   - checkChildren: getCustomerByChildCode, customerIsActive, childIsActive   - saveConsumptions(isRectification=false)

--- internal/domain/services/billing/insert_consumption.go
+++ internal/domain/services/billing/insert_consumption.go
@@ -2,6 +2,7 @@ package billing
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/pjover/sam/internal/domain/model"
 	"github.com/pjover/sam/internal/domain/ports"
 	"github.com/pjover/sam/internal/domain/services/common"
@@ -22,13 +23,26 @@ func NewBillingService(dbService ports.DbService) BillingService {
 	}
 }
 
-func (i billingService) InsertConsumptions(code int, consumptions map[string]float64, note string) (string, error) {
+func (i billingService) InsertConsumptions(childCode int, consumptions map[string]float64, note string) (string, error) {
 	var buffer bytes.Buffer
 
-	child, err := i.dbService.FindChild(code)
+	child, err := i.dbService.FindChild(childCode)
 	if err != nil {
 		return "", err
 	}
+	if !child.Active {
+		return "", fmt.Errorf("l'infant %s no està activat, edita'l per activar-lo abans d'insertar consums", child.NameWithCode())
+	}
+
+	customerCode := childCode / 10
+	customer, err := i.dbService.FindCustomer(customerCode)
+	if err != nil {
+		return "", err
+	}
+	if !customer.Active {
+		return "", fmt.Errorf("el client %s no està activat, edita'l per activar-lo abans d'insertar consums", customer.FirstAdultNameWithCode())
+	}
+
 	products, err := i.dbService.FindAllProducts()
 	if err != nil {
 		return "", err
@@ -39,11 +53,12 @@ func (i billingService) InsertConsumptions(code int, consumptions map[string]flo
 	var completeConsumptions []model.Consumption
 	for id, units := range consumptions {
 		c := model.Consumption{
-			Code:      common.RandString(model.ConsumptionCodeLength),
-			ChildCode: code,
-			ProductID: id,
-			Units:     units,
-			YearMonth: yearMonth,
+			Code:            common.RandString(model.ConsumptionCodeLength),
+			ChildCode:       childCode,
+			ProductID:       id,
+			Units:           units,
+			YearMonth:       yearMonth,
+			IsRectification: false,
 		}
 		if first {
 			c.Note = note
