b6a7b83eb07138dc0cea52898c0a85c049d9d95e
Pere Jover <pjover@gmail.com>
2022-07-27 20:28:28 +0200

    Change database type from primitive.Decimal128 to numberDouble

--- internal/adapters/mongo_db/dbo/product.go
+++ internal/adapters/mongo_db/dbo/product.go
@@ -1,12 +1,10 @@
 package dbo
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type Product struct {
-	Id            string               `bson:"_id"`
-	Name          string               `bson:"name"`
-	ShortName     string               `bson:"shortName"`
-	Price         primitive.Decimal128 `bson:"price"`
-	TaxPercentage primitive.Decimal128 `bson:"taxPercentage"`
-	IsSubsidy     bool                 `bson:"isSubsidy"`
+	Id            string  `bson:"_id"`
+	Name          string  `bson:"name"`
+	ShortName     string  `bson:"shortName"`
+	Price         float64 `bson:"price"`
+	TaxPercentage float64 `bson:"taxPercentage"`
+	IsSubsidy     bool    `bson:"isSubsidy"`
 }
